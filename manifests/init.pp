da9dff420698288d0d8d91bb2947ef212f473791
Marcel Haerry <haerry@puzzle.ch>
2008-05-26 20:11:12 +0200

    removed debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,7 +58,6 @@ class shorewall {
 	define entry ($line) {
 		$target = "/var/lib/puppet/modules/shorewall/${name}"
 		$dir = dirname($target)
-        notice("debug: $dir")
 		file { $target:
 			content => "${line}\n",
 			mode => 0600, owner => root, group => 0,
