7d1754d8207497d0b6996ce8466da6c2b70adece
Udo Waechter <root@zoide.net>
2011-07-25 17:51:57 +0200

    initial import

---
+++ shorewall/manifests/debian.pp
@@ -0,0 +1,15 @@
+class shorewall::debian inherits shorewall::base {
+    case $shorewall_startup {
+      '': { $shorewall_startup = "1" }
+    }
+    file{'/etc/default/shorewall':
+        #source => "puppet:///modules/shorewall/debian/default",
+        content => template("shorewall/debian_default.erb"),
+        require => Package['shorewall'],
+        notify => Service['shorewall'],
+        owner => root, group => 0, mode => 0644;
+    }
+    Service['shorewall']{
+        status => '/sbin/shorewall status'
+    }
+}
