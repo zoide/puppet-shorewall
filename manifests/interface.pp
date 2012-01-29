8a7cc58ae2f94708c53aab09ca59ff4f1cdc12a8
root <root@maketank.net>
2012-01-29 18:16:20 +0100

    introducing interface parameter

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -1,27 +1,22 @@
-define shorewall::interface(
-    $zone,
+define shorewall::interface ($zone,
+    $interface = '',
     $broadcast = 'detect',
     $options = 'tcpflags,blacklist,routefilter,nosmurfs,logmartians',
-    $rfc1918 = false,
     $dhcp = false,
-    $order = 100
-){
-    if $rfc1918 {
-        if $dhcp {
-            $options_real = "${options},dhcp"
-        } else {
-            $options_real = $options
-        }
-    } else {
-        if $dhcp {
-            $options_real = "${options},norfc1918,dhcp"
-        } else {
-            $options_real = "${options},norfc1918"
-        }
+    $order = 100) {
+    if $dhcp {
+        $options_real = "${options},dhcp"
     }
-
-    shorewall::entry { "interfaces.d/${order}-${name}":
-        line => "${zone} ${name} ${broadcast} ${options_real}",
+    else {
+        $options_real = $options
+    }
+    $iface_real = $interface ? {
+        '' => $name,
+        default => $interface,
+    }
+    shorewall::entry {
+        "interfaces.d/${order}-${name}" :
+            line => "${zone} ${iface_real} ${broadcast} ${options_real}",
     }
 }
 
