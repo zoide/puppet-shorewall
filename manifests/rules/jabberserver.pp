a9b993d8821e501f4b45f45c2a162f6340fb59fe
do <udo@maketank.net>
2013-10-12 17:30:16 +0200

    adding destination param

--- manifests/rules/jabberserver.pp
+++ manifests/rules/jabberserver.pp
@@ -1,19 +1,22 @@
-class shorewall::rules::jabberserver {
+class shorewall::rules::jabberserver (
+  $destination = '$FW',
+  $source      = '$FW') {
   shorewall::rule {
     'net-me-tcp_jabber':
-            source          => 'net',
-            destination     => '$FW',
-            proto           => 'tcp',
-            destinationport => '5222,5223,5269',
-            order           => 240,
-            action          => 'ACCEPT';
+      source          => 'net',
+      destination     => $destination,
+      proto           => 'tcp',
+      destinationport => '5222,5269,5280',
+      order           => 240,
+      action          => 'ACCEPT';
+
     'me-net-tcp_jabber_s2s':
-            source          => '$FW',
-            destination     => 'net',
-            proto           => 'tcp',
-            destinationport => '5260,5269,5270,5271,5272',
-            order           => 240,
-            action          => 'ACCEPT';
+      source          => $source,
+      destination     => 'net',
+      proto           => 'tcp',
+      destinationport => '5222,5269,5280',
+      order           => 240,
+      action          => 'ACCEPT';
   }
 
 }
