88a2150fbe31cdff4fb99a9bf4bb6b357ea380f7
Udo Waechter <udo@maketank.net>
2015-08-18 14:37:29 +0200

    origindest parameter

--- manifests/rules/http.pp
+++ manifests/rules/http.pp
@@ -1,10 +1,11 @@
-class shorewall::rules::http ($destination="\$FW") {
-    shorewall::rule { 'net-me-http-tcp':
-        source          => 'net',
-        destination     => $destination,
-        proto           => 'tcp',
-        destinationport => '80',
-        order           => 240,
-        action          => 'ACCEPT';
-    }
+class shorewall::rules::http ($destination = "\$FW", $originaldest = '-') {
+  shorewall::rule { 'net-me-http-tcp':
+    source          => 'net',
+    destination     => $destination,
+    proto           => 'tcp',
+    destinationport => '80',
+    order           => 240,
+    action          => 'ACCEPT',
+    originaldest    => $originaldest;
+  }
 }
--- manifests/rules/https.pp
+++ manifests/rules/https.pp
@@ -1,10 +1,11 @@
-class shorewall::rules::https ($destination="\$FW"){
-    shorewall::rule { 'net-me-https-tcp':
-        source          => 'net',
-        destination     => $destination,
-        proto           => 'tcp',
-        destinationport => '443',
-        order           => 240,
-        action          => 'ACCEPT';
-    }
+class shorewall::rules::https ($destination = "\$FW", $originaldest = '-',) {
+  shorewall::rule { 'net-me-https-tcp':
+    source          => 'net',
+    destination     => $destination,
+    proto           => 'tcp',
+    destinationport => '443',
+    order           => 240,
+    originaldest    => $originaldest,
+    action          => 'ACCEPT';
+  }
 }
